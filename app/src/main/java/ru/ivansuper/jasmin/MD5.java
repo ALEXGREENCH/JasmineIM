277aa444070245d508b620efd55404384cc84eb5
ALEXGREENCH <greench4alex@gmail.com>
2025-07-24 23:55:34 +0300

    Use local MessageDigest for MD5

--- app/src/main/java/ru/ivansuper/jasmin/MD5.java
+++ app/src/main/java/ru/ivansuper/jasmin/MD5.java
@@ -5,17 +5,18 @@ import java.security.MessageDigest;
 /** @noinspection ALL*/
 public class MD5 {
     public static final byte[] AIM_MD5_STRING = "AOL Instant Messenger (SM)".getBytes();
-    private static MessageDigest md5;
+
     public static byte[] calculateMD5(byte[] bytesToHash) {
-        return md5.digest(bytesToHash);
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            return md5.digest(bytesToHash);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return new byte[0];
+        }
     }
 
     public static void init() {
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
+        // No initialization required anymore
     }
 }
